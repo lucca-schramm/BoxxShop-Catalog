e6477ed72d8c691f8474897182b1d973793b9564
Lucca Schramm <luccascho@gmail.com>
2023-05-29 15:39:58 -0300

    Adicionando categoria e descrição do produto para possibilitar filtragem

--- src/components/PhotoItem/index.tsx
+++ src/components/PhotoItem/index.tsx
@@ -3,14 +3,20 @@ import * as C from './styles';
 type Props = {
     url: string;
     name: string;
+    description: string;
+    category: string;
     onDelete: (name: string) => void;
 }
 
-export const PhotoItem =  ({url, name, onDelete }: Props) => {
+export const PhotoItem =  ({url, name, description, category, onDelete }: Props) => {
     return (
         <C.Container>
             <img src={url} alt={name}/>
-            {name}
+            Produto: {name}
+            <br/>
+            Descrição: { description }
+            <br />
+            Categoria: { category }
             <button onClick={()=> onDelete(name)}> Excluir </button>
         </C.Container>
     );
--- src/components/PhotoItem/styles.ts
+++ src/components/PhotoItem/styles.ts
@@ -1,13 +1,31 @@
 import styled from "styled-components"
+
+
 export const Container = styled.div`
     background-color:  #3D3F43;
     border-radius: 10px;
     padding: 10px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
 
     img{
         max-width: 100%;
         display: block;
         margin-bottom: 10px;
         border-radius: 10px;
+
+        &:hover{
+            transform: scale(1.5);
+        }
     }
+    button{
+        margin-top:5px;
+    }
+
+    &:hover {
+        transform: scale(1.1);
+        }
 `;
