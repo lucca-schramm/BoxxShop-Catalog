02c0ebde2e504cb5176595b75ad08dd037b32808
Lucca Schramm <luccascho@gmail.com>
2023-06-14 15:06:42 -0300

    Lógica de botão do whatsapp

---
+++ src/components/PhotoItem/buttonWhatsapp.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+type ProductButtonProps = {
+  name: string;
+  description: string;
+  url: string;
+};
+
+const ProductButton: React.FC<ProductButtonProps> = ({ name, description, url }) => {
+  const handleClick = () => {
+    const whatsappNumber = '+5571987052690';
+    const message = `Olá, vim do catálogo e gostaria de encomendar o produto ${name}, ${description}.`;
+
+    const encodedMessage = encodeURIComponent(message);
+    const encodedImageUrl = encodeURIComponent(url);
+
+    const whatsappUrl = `https://wa.me/${whatsappNumber}?text=${encodedMessage}%0a%0a${encodedImageUrl}`;
+
+    window.open(whatsappUrl, '_blank');
+  };
+
+  return (
+    <button onClick={handleClick}>Abrir Conversa do WhatsApp</button>
+  );
+};
+
+export default ProductButton;
--- src/components/PhotoItem/index.tsx
+++ src/components/PhotoItem/index.tsx
@@ -1,34 +1,53 @@
 import * as C from './styles';
+import React from 'react';
+import ProductButton from './buttonWhatsapp';
 
 type Props = {
-    url: string;
-    name: string;
-    description: string;
-    category: string;
-    league: string;
-    brand: string;
-    dataModificacao: Date,
-    onDelete: (name: string) => void;
-}
+  url: string;
+  name: string;
+  description: string;
+  category: string;
+  league: string;
+  brand: string;
+  dataModificacao: Date;
+  onDelete: (name: string) => void;
+};
 
-export const PhotoItem =  ({url, name, description, category, league, dataModificacao, brand, onDelete }: Props) => {
-    return (
-        <C.Container>
-            <img src={url} alt={name}/>
-            <div>
-                Produto:<span> { name } </span>
-                <br/>
-                Descrição:<span> { description } </span>
-                <br />
-                Categoria:<span> { category } </span>
-                <br />
-                Liga: <span> { league } </span>
-                <br />
-                Marca: <span>{ brand }</span>
-                <br />
-                Adicionado: <span> { dataModificacao?.getDate()+'/'+dataModificacao?.getMonth()+'/'+dataModificacao?.getFullYear() }</span>
-            </div>
-                <button onClick={()=> onDelete(name)}> Excluir </button>
-        </C.Container>
-    );
-}
+export const PhotoItem = ({
+  url,
+  name,
+  description,
+  category,
+  league,
+  dataModificacao,
+  brand,
+  onDelete
+}: Props) => {
+  return (
+    <C.Container>
+      <img src={url} alt={name} />
+      <div>
+        Produto:<span> {name} </span>
+        <br />
+        Descrição:<span> {description} </span>
+        <br />
+        Categoria:<span> {category} </span>
+        <br />
+        Liga: <span> {league} </span>
+        <br />
+        Marca: <span>{brand}</span>
+        <br />
+        Adicionado:{' '}
+        <span>
+          {dataModificacao?.getDate() +
+            '/' +
+            dataModificacao?.getMonth() +
+            '/' +
+            dataModificacao?.getFullYear()}
+        </span>
+      </div>
+      <button onClick={()=> onDelete(name)}>Excluir</button>
+      <ProductButton name={name} description={description} url={url} />
+    </C.Container>
+  );
+};
