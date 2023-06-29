c8220dd9f7f8af595980831721a5229da9d312dc
Lucca Schramm <luccascho@gmail.com>
2023-06-29 20:10:39 -0300

    feat: upgrade on filter

--- src/components/PhotoList/PhotoList.tsx
+++ src/components/PhotoList/PhotoList.tsx
@@ -1,4 +1,3 @@
-// PhotoList.tsx
 import React from 'react';
 import * as C from './styles';
 import { PhotoItem } from '../PhotoItem';
@@ -11,27 +10,37 @@ interface PhotoListProps {
 }
 
 const PhotoList: React.FC<PhotoListProps> = ({ photos, filter, handleDeleteClick }) => {
+  const applyFilters = (item: Photo) => {
+    const { name, category, description, league } = item;
+    const lowercaseFilter = filter.toLowerCase();
+
+    const filters = lowercaseFilter.split(" ");
+
+    const filterMatches = filters.every(filter => {
+      const itemString = `${name.toLowerCase()} ${description.toLowerCase()} ${category.toLowerCase()} ${league.toLowerCase()}`;
+      return itemString.includes(filter);
+    });
+  
+    return filterMatches;
+  };
+
+  const filteredPhotos = photos.filter(applyFilters);
+
   return (
     <C.PhotoList>
-      {photos
-        .filter((item) =>
-          item?.name.toLowerCase().includes(filter.toLowerCase()) ||
-          item?.category.toLowerCase().includes(filter.toLowerCase()) ||
-          item?.league.toLowerCase().includes(filter.toLowerCase())
-        )
-        .map((item, index) => (
-          <PhotoItem
-            key={index}
-            url={item.url}
-            name={item.name}
-            description={item.description}
-            category={item.category}
-            league={item.league}
-            brand={item.brand}
-            dataModificacao={item.dateModify}
-            onDelete={handleDeleteClick}
-          ></PhotoItem>
-        ))}
+      {filteredPhotos.map((item, index) => (
+        <PhotoItem
+          key={index}
+          url={item.url}
+          name={item.name}
+          description={item.description}
+          category={item.category}
+          league={item.league}
+          brand={item.brand}
+          dataModificacao={item.dateModify}
+          onDelete={handleDeleteClick}
+        />
+      ))}
     </C.PhotoList>
   );
 }
